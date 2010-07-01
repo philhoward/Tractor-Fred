a299dc28347de0c0473ab7dd13db39744b52f198
philhoward <pwh@cecs.pdx.edu>
2010-07-01 12:20:03 -0700

    initial release of schematic and some source code.

---
+++ source/fred.c
@@ -0,0 +1,258 @@
+/*
+The MIT License
+
+Copyright (c) 2010 Philip W. Howard
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include "fred.h"
+
+//***************************************************************************
+//Main() - Main Routine
+//***************************************************************************
+void main()
+{
+	Init();                              //Initialize 12F629 Microcontroller
+	while(1)                             //Loop Forever
+	{	
+		Delay(ontime);
+		Led1();
+		Delay(offtime);
+		Led2();
+		
+		/*
+		if (ontime & 0x01) Led1();
+		if (ontime & 0x02) Led2();
+		if (ontime & 0x04) Led3();
+		if (ontime & 0x08) Led4();
+		if (ontime & 0x10) Led5();
+		if (ontime & 0x20) Led6();
+		if (ontime & 0x40) Led7();
+		if (ontime & 0x80) Led8();
+		*/
+		
+		if ((ADCON0 & 0x02) == 0)
+		{
+			ontime = ADRESH;
+			offtime = 255-ontime;
+			ADCON0 = 0x03;
+		} 
+	}
+}
+
+
+//***************************************************************************
+//Init - Initialization Routine
+//***************************************************************************
+void Init()
+{
+	#asm 								 //Load Factory Calibration Value Into OSCCAL
+		call 0x3FF
+		bsf	_STATUS,5
+		movwf _OSCCAL
+	#endasm
+	TRISIO = 0xFF;                       //Set All I/O's As Inputs
+	GPIO = CLEAR;                        //Clear GPIO
+	VRCON = CLEAR;                       //Turn Off Voltage Reference Peripheral
+	CMCON = 0x07;                        //Turn Off Comparator Peripheral
+	TMR0 = CLEAR;                        //Clear Timer0
+	OPTION = 0x80;
+	ANSEL = 0x31;						 //RA0 Analog Input
+	ADCON0 = 0x01;
+	//IOCB3 = SET;                         //GP3 Interrupt On Pin Changed Enabled
+	//GPIE = SET;                          //Interrupt On Pin Change Enabled
+	//T0IE = SET;                          //Timer0 Overflow Interrupt Enabled
+	//ADIE = SET;
+	//ADIF = CLEAR;
+	//T0IF = CLEAR;                        //Clear Timer0 Overflow Interrupt Flag
+	//GPIF = CLEAR;                        //Clear Interrupt On Pin Change Flag
+	//GIE = SET;                           //Enable All Interrupts
+
+	ontime = 0;
+	offtime = 255;
+	return;
+}
+
+//***************************************************************************
+//Functions
+//***************************************************************************
+
+//***************************************************************************
+//Led1 - D0
+//***************************************************************************
+void Led1()                              
+{
+	TRISIO = LED1TRIS;
+	GPIO = LED1ON;
+	return;
+}
+
+//***************************************************************************
+//Led2 - D1
+//***************************************************************************
+void Led2()				 				 
+{
+	TRISIO = LED2TRIS;
+	GPIO = LED2ON;
+	return;
+}
+//***************************************************************************
+//Led3 - D2
+//***************************************************************************
+void Led3()                              
+{
+	TRISIO = LED3TRIS;
+	GPIO = LED3ON;
+	return;
+}
+
+//***************************************************************************
+//Led4 - D3
+//***************************************************************************
+void Led4()								 
+{
+	TRISIO = LED4TRIS;
+	GPIO = LED4ON;
+	return;
+}
+
+//***************************************************************************
+//Led5 - D4
+//***************************************************************************
+void Led5()								 
+{
+	TRISIO = LED5TRIS;
+	GPIO = LED5ON;
+	return;
+}
+
+//***************************************************************************
+//Led6 - D5
+//***************************************************************************
+void Led6()								 
+{
+	TRISIO = LED6TRIS;
+	GPIO = LED6ON;
+	return;
+}
+
+//***************************************************************************
+//Led7() - D6
+//***************************************************************************
+void Led7()                              
+{
+	TRISIO = LED7TRIS;
+	GPIO = LED7ON;
+	return;
+}
+//***************************************************************************
+//Led8() - D7
+//***************************************************************************
+void Led8()                              
+{
+	TRISIO = LED8TRIS;
+	GPIO = LED8ON;
+	return;
+}
+
+//***************************************************************************
+//Delay(value) - Delay Routine
+//             - Delay=value*2.3ms (When OSC=4MHZ)
+//***************************************************************************
+void Delay(char value)                    
+{
+	for (outer=value; outer != 0; outer--)	 
+	{
+		for (inner=0xFF; inner != 0; inner--)
+		{
+	  
+		}
+	}
+	return;
+}
+
+//***************************************************************************
+//Isr() - Interrupt Service Routine
+//      - Services Timer0 Overflow
+//      - Services GP3 Pin Change
+//***************************************************************************
+void interrupt Isr()
+{
+/*
+	if ((T0IE & T0IF) == SET)			  //If A Timer0 Interrupt, Then
+	{
+		Display();						  //Update LED Array
+		GODONE = 1;						  //Start an A/D Conversion
+		T0IF = CLEAR;                     //Clear Timer0 Interrupt Flag
+	}
+	
+	if ((GPIE & GPIF) == SET)					  //If A GP3 Pin-Change Interrupt
+	{
+		if (Debounce() == TRUE)     		 //Debounce Pushbutton
+			leddirection ^= 0x1;
+			      					     //Toggle Timer0 Prescaler (Changes LED Flashing Speed)
+		GPIF = CLEAR;                    //Clear Interrupt On Pin Change Flag
+	}
+	
+	if ((ADIE & ADIF) == SET)			//Binary search used to change timer0 prescaler
+	{
+									    //which is used for led speed control
+		if (ADRESH < 0x7F)
+		{
+			if (ADRESH > 0x3F)
+			{
+				if (ADRESH > 0x5E)
+					OPTION = 0x84;
+				else				//ADRESH < 0X5E
+					OPTION = 0x85;
+			}
+				
+			else 					//ADRESH < 0X3F
+			{	if (ADRESH > 0x1F)
+					OPTION = 0x86;
+				else 				//ADRESH < 0X1F
+					OPTION = 0x87;
+			}		
+					
+		}
+		else  						//ADRESH > 0x7F
+		{
+			if (ADRESH > 0xBE)
+			{
+				if (ADRESH > 0xDD)
+					OPTION = 0x80;	
+				else				//ADRESH < 0XDD
+					OPTION = 0x81;				
+			}
+			else				    //ADRESH < 0XBE
+			{
+				if (ADRESH > 0x9F)
+					OPTION = 0x82;
+				else				//ADRESH < 0X9F
+					OPTION = 0x83;
+			}	
+		}
+
+		delaytime = ADRESH;
+		ADIF = CLEAR;				//Clear A/D Interrupt Flag
+	}
+*/
+	return;
+}
---
+++ source/fred.h
@@ -0,0 +1,82 @@
+/*
+The MIT License
+
+Copyright (c) 2010 Philip W. Howard
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#ifndef TEST_H
+#define TEST_H
+
+#include <pic.h>
+#include <pic12f6x.h>
+
+__IDLOC(1);
+__CONFIG(INTIO & WDTDIS & MCLRDIS & BORDIS & UNPROTECT & PWRTEN);
+
+//Defines
+
+#define	LED1TRIS	0b11001111
+#define	LED2TRIS	0b11001111
+#define	LED3TRIS	0b11101011
+#define	LED4TRIS	0b11101011
+#define	LED5TRIS	0b11011011
+#define	LED6TRIS	0b11011011
+#define	LED7TRIS	0b11111001
+#define	LED8TRIS	0b11111001
+#define LED1ON		0b00010000
+#define	LED2ON		0b00100000
+#define	LED3ON		0b00010000
+#define	LED4ON		0b00000100
+#define	LED5ON		0b00100000
+#define	LED6ON		0b00000100
+#define	LED7ON		0b00000100
+#define	LED8ON		0b00000010
+#define	PUSHBUTTON	 	GPIO3		//GP3 - Pushbutton Input
+#define	DEBOUNCEDELAY	200		    //Debounce Delay Constant
+#define	OPEN			1
+#define	CLOSED			0
+#define	SET				1
+#define	CLEAR			0
+#define	FIRSTSTATE		0
+#define LASTSTATE		7
+#define	TRUE			1
+#define	FALSE			0
+#define FORWARD			0
+
+//Global Variable Declarations
+unsigned char inner,outer;
+unsigned char ontime;
+unsigned char offtime;
+
+//Function Prototypes
+void Init();
+void Led1();
+void Led2();
+void Led3();
+void Led4();
+void Led5();
+void Led6();
+void Led7();
+void Led8();
+void Delay(char value);
+void Display();
+
+#endif
